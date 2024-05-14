867129c281bf9940947ccfb7aff94fb86218a6ee
Douglas Trindade <douglastrindade22@hotmail.com>
2024-05-14 00:04:09 -0300

    feat: product form

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,5 +1,47 @@
+import { Button } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import Link from "next/link"
+import { PageHeader } from "../_components/PageHeader"
+
 const AdminProductsPage = () => {
-  return <div>Products</div>
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+      </div>
+      <ProductsTable />
+    </>
+  )
+}
+
+const ProductsTable = () => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody></TableBody>
+    </Table>
+  )
 }
 
 export default AdminProductsPage
